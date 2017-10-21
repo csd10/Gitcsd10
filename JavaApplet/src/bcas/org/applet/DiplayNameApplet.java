65a34aa0763f8acffd9d1dd5487e90aa2dc177cd
BCAS CSD-8 <31151574+bcascsd8@users.noreply.github.com>
2017-10-21 13:50:06 +0530

    First Commit

---
+++ JavaApplet/src/bcas/org/applet/DiplayNameApplet.java
@@ -0,0 +1,11 @@
+package bcas.org.applet;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class DiplayNameApplet extends Applet {
+
+	public void paint(Graphics g) {
+		g.drawString("BCAS", 20, 20);
+	}
+}
