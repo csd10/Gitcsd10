0d494df590eef9df022d06dd7c7af4d46a20d899
csd10 <32977220+csd10@users.noreply.github.com>
2017-10-21 13:59:23 +0530

    Update DiplayNameApplet.java

--- JavaApplet/src/bcas/org/applet/DiplayNameApplet.java
+++ JavaApplet/src/bcas/org/applet/DiplayNameApplet.java
@@ -6,6 +6,6 @@ import java.awt.Graphics;
 public class DiplayNameApplet extends Applet {
 
 	public void paint(Graphics g) {
-		g.drawString("BCAS Jaffna", 20, 20);
+		g.drawString("BCAS Jaffna Cmpus", 20, 20);
 	}
 }
